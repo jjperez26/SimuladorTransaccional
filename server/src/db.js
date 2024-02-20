c23b4f29cb169e2ef46771564595f905845f35e9
lennyz0689 <lennyz0689@gmail.com>
2024-02-20 18:41:22 -0500

    Create model}

--- server/src/db.js
+++ server/src/db.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const modelUser = require('./models/user')
 
 const sequelize = new Sequelize(
    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
@@ -9,6 +10,11 @@ const sequelize = new Sequelize(
       native: false,
    }
 );
+
+modelUser(sequelize)
+
+
+
 module.exports = {
    ...sequelize.models, 
    conn: sequelize,
