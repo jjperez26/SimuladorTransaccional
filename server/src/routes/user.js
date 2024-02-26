63fedb0b7594069541f44c2a33c97c3e57006572
jmartinezes <johan220hola@gmail.com>
2024-02-25 23:01:48 -0500

    peticiones

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -4,7 +4,7 @@ const { user, login, usersList } = require('../handlers/users')
 const users = Router()
 
 users.get('/', usersList)
-users.get('/login', login)
+users.post('/login', login)
 users.post('/crearUsuario', user)
 
 module.exports = { users }
