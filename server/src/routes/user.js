0cb79c5c46e685413eb3f51d841e04446b7eb7b6
jmartinezes <johan220hola@gmail.com>
2024-02-25 23:08:30 -0500

    25Feb1108

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -4,10 +4,6 @@ const { user, login, usersList, editUser } = require('../handlers/users')
 const users = Router()
 
 users.get('/', usersList)
-<<<<<<< HEAD
-users.put('/', editUser)
-=======
->>>>>>> 63fedb0b7594069541f44c2a33c97c3e57006572
 users.post('/login', login)
 users.post('/crearUsuario', user)
 
