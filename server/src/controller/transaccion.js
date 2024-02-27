8114ff6557096866a0ded694bbdc2181c84c39c5
lennyz0689 <lennyz0689@gmail.com>
2024-02-27 13:41:54 -0500

    create endpoint logic transaccion

--- server/src/controller/transaccion.js
+++ server/src/controller/transaccion.js
@@ -1,14 +1,56 @@
-const axios = require('axios')
-const { transacciones } = require('../db')
+const { transacciones, usuarios, conn } = require('../db');
 
 const crearTransaccion = async (CUENTA_ORIGEN, CUENTA_DESTINO, VALOR_TRANSACCION) => {
+    const validarDestino = await usuarios.findOne({
+        where: {
+            NUMERO_CELULAR: CUENTA_DESTINO
+        }
+    });
+
+    if (!validarDestino) {
+        throw new Error('El número de destino no se encuentra registrado');
+    }
+
+    const validarSaldo = await usuarios.findOne({
+        where: {
+            NUMERO_CELULAR: CUENTA_ORIGEN
+        }
+    });
+
+    if (!validarSaldo) {
+        throw new Error('La cuenta de origen no se encuentra registrada');
+    }
+
+    if (validarSaldo.SALDO < VALOR_TRANSACCION) {
+        throw new Error('El saldo tiene que ser menor a la transacción');
+    }
+
     const newTransaccion = await transacciones.create({
         CUENTA_ORIGEN: CUENTA_ORIGEN,
         CUENTA_DESTINO: CUENTA_DESTINO,
         VALOR_TRANSACCION: VALOR_TRANSACCION,
-    })
+    });
 
-    return newTransaccion
-}
+    // Restar el valor de la transacción de la cuenta de origen
+    await usuarios.update({
+        SALDO: validarSaldo.SALDO - VALOR_TRANSACCION
+    }, {
+        where: {
+            NUMERO_CELULAR: CUENTA_ORIGEN
+        },
+    });
 
-module.exports = { crearTransaccion }
+    // Sumar el valor de la transacción a la cuenta de destino
+    await usuarios.update({
+        SALDO: validarDestino.SALDO + VALOR_TRANSACCION
+    }, {
+        where: {
+            NUMERO_CELULAR: CUENTA_DESTINO
+        },
+    });
+
+    
+    return newTransaccion;
+};
+
+module.exports = { crearTransaccion };
